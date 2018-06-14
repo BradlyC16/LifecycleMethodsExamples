7aabf2754e37353e91dd3199ae219c74f06c71ad
Bradly Cockrell <djbradlythehedgehog@gmail.com>
2018-06-14 11:06:05 -0400

    Completed examples

--- app/src/main/java/com/example/bradleycockrell/lifecyclemethodexaples/MainActivity.java
+++ app/src/main/java/com/example/bradleycockrell/lifecyclemethodexaples/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bradleycockrell.lifecyclemethodexaples;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,18 +19,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Create small popup at the bottom of the screen
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
 
+//        Links the variable reference to a specific UI element by id (name)
         newActivityButton = findViewById(R.id.new_activity_button);
         finishActivityButton = findViewById(R.id.finish_activity_button);
 
+//        Setting Intent to navigate from THIS activity to a different one
         newActivity = new Intent(this, SecondActivity.class);
 
+//        Method to initiate onClicksListeners for the buttons in the UI
         setListeners();
     }
 
     private void setListeners() {
-        newActivityButton.setOnClickListener();
+//        Sets listener for newActivityButton only
+        newActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Takes our Intent and tells it to use to start a new Activity
+                startActivity(newActivity);
+
+            }
+        });
+
+//        Sets listener For finishProgramButton only
+        finishActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Closes current activity (in this location this call will close our program)
+                finish();
+
+            }
+        });
     }
 
     @Override
